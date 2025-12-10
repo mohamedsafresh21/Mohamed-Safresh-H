05ed3165512d75c8d219a3cf23d321d6d11609be
mohamedsafresh21 <104716438+mohamedsafresh21@users.noreply.github.com>
2025-12-10 13:56:07 +0530

    Create  program-2.java

---
+++ program-2.java
@@ -0,0 +1,9 @@
+public class OddSeries {
+    public static void main(String[] args) {
+        int a = 5;  // example input
+
+        for (int i = 1; i <= a; i++) {
+            System.out.print((2 * i - 1) + " ");
+        }
+    }
+}
