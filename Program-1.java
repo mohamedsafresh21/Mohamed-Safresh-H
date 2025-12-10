d80d9a40e25bf31b66c7d21adbf77fda06d82fc2
mohamedsafresh21 <104716438+mohamedsafresh21@users.noreply.github.com>
2025-12-10 13:53:49 +0530

    Create Program-1.java

---
+++ Program-1.java
@@ -0,0 +1,37 @@
+class Calculator {
+    double a, b;
+    String operation;
+
+    Calculator(double a, double b, String operation) {
+        this.a = a;
+        this.b = b;
+        this.operation = operation;
+    }
+
+    public double calculate() {
+        switch (operation.toLowerCase()) {
+            case "add":
+                return a + b;
+            case "sub":
+                return a - b;
+            case "mul":
+                return a * b;
+            case "div":
+                if (b == 0) {
+                    System.out.println("Cannot divide by zero");
+                    return 0;
+                }
+                return a / b;
+            default:
+                System.out.println("Invalid operation");
+                return 0;
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator c = new Calculator(10, 5, "add");
+        System.out.println("Result = " + c.calculate());
+    }
+}
